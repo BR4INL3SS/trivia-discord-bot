286482cd5f6cbca9ef0653f2476e6d6ce9056d5c
Mehdi Hyani <m.hyani@aui.ma>
2022-06-26 20:39:52 +0100

    refactor: added separate slash commands deployment file

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,7 @@
-import { REST } from "@discordjs/rest";
 import dotenv from "dotenv";
 import fs from "fs";
 import { discordClient } from "./utils/discord";
 import {
-  Routes,
   RESTPostAPIApplicationCommandsJSONBody,
 } from "discord-api-types/v9";
 import { SlashCommandBuilder } from "@discordjs/builders";
@@ -26,28 +24,8 @@ for (const file of commandFiles) {
   commands.push(command["default"].toJSON());
 }
 
-// Place your client and guild ids here
-const clientId = process.env.CLIENT_ID || "";
-const guildId = process.env.GUILD_ID || "";
-
-const commandPrefix = "br4!";
-
-discordClient.on("ready", async () => {
-  console.info("BR4 BOT v2 is up and running!");
-
-  const rest = new REST({ version: "9" }).setToken(
-    process.env.DISCORD_TOKEN || ""
-  );
-
-  try {
-    console.log("Started refreshing application (/) commands.");
-
-    await rest.put(Routes.applicationCommands(clientId), { body: commands });
-
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
+discordClient.on("ready", async (client) => {
+  console.info(`${client.user.tag} is up and running!`);
 });
 
 discordClient.on(
